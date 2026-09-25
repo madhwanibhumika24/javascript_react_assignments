b5d796d44a6aa2ec60b1a487cd6909ed6d454947
Bhumika Madhwani <madhwanibhumika24@gmail.com>
2026-09-25 12:12:03 +0530

    Add calculator operations

---
+++ 1. JavaScript Calculator using Functions/script.js
@@ -0,0 +1,32 @@
+function calculate() {
+
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    let operator = document.getElementById("operator").value;
+
+    let result;
+
+    switch (operator) {
+
+        case "+":
+            result = num1 + num2;
+            break;
+
+        case "-":
+            result = num1 - num2;
+            break;
+
+        case "*":
+            result = num1 * num2;
+            break;
+
+        case "/":
+            result = num1 / num2;
+            break;
+
+        default:
+            result = "Invalid operation";
+    }
+
+    document.getElementById("result").innerText = result;
+}
